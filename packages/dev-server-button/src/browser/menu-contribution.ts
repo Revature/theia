536a69ea133efdfe2973a3d632c1d866b5d4d0ac
RevvingUpThatHill <theodore.balashov@revature.com>
2025-05-01 13:32:00 -0400

    added button which invokes the openDevServer command from the cloud ide extension

---
+++ packages/dev-server-button/src/browser/menu-contribution.ts
@@ -0,0 +1,88 @@
+// *****************************************************************************
+// Copyright (C) 2020 TORO Limited and others.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v. 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0.
+//
+// This Source Code may also be made available under the following Secondary
+// Licenses when the conditions for such availability set forth in the Eclipse
+// Public License v. 2.0 are satisfied: GNU General Public License, version 2
+// with the GNU Classpath Exception which is available at
+// https://www.gnu.org/software/classpath/license.html.
+//
+// SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-only WITH Classpath-exception-2.0
+// *****************************************************************************
+
+import { QuickInputService } from '@theia/core/lib/browser';
+import {
+    Command, CommandContribution, CommandRegistry, MAIN_MENU_BAR,
+    MenuContribution, MenuModelRegistry, MenuNode, MessageService, SubMenuOptions
+} from '@theia/core/lib/common';
+import { inject, injectable, interfaces } from '@theia/core/shared/inversify';
+
+const DevServerCommand: Command = {
+    id: 'dev-server-button',
+    label: 'Open port in browser...'
+};
+
+@injectable()
+export class BrowserMenuCommandContribution implements CommandContribution {
+
+    @inject(QuickInputService)
+    protected readonly quickInputService: QuickInputService;
+
+    @inject(MessageService)
+    protected readonly messageService: MessageService;
+
+    registerCommands(commands: CommandRegistry): void {
+
+        commands.registerCommand(DevServerCommand, {
+            execute: () => {
+                commands.executeCommand('cloud-ide-extension.openDevServer');
+            }
+        });
+    }
+
+}
+
+@injectable()
+export class BrowserMenuContribution implements MenuContribution {
+    registerMenus(menus: MenuModelRegistry): void {
+        setTimeout(() => {
+
+            const subMenuPath = [...MAIN_MENU_BAR, 'DevServerMenu'];
+            menus.registerSubmenu(subMenuPath, 'Browser', {
+                order: 'zzz'
+            });
+            menus.registerMenuAction(subMenuPath, {
+                commandId: DevServerCommand.id,
+                order: '0'
+            });
+
+        }, 10000);
+    }
+
+}
+
+/**
+ * Special menu node that is not backed by any commands and is always disabled.
+ */
+export class PlaceholderMenuNode implements MenuNode {
+
+    constructor(readonly id: string, public readonly label: string, protected options?: SubMenuOptions) { }
+
+    get icon(): string | undefined {
+        return this.options?.iconClass;
+    }
+
+    get sortString(): string {
+        return this.options?.order || this.label;
+    }
+
+}
+
+export const bindSampleMenu = (bind: interfaces.Bind) => {
+    bind(CommandContribution).to(BrowserMenuCommandContribution).inSingletonScope();
+    bind(MenuContribution).to(BrowserMenuContribution).inSingletonScope();
+};
